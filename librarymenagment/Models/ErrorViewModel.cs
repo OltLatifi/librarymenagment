c8cca4f4e25506539524f2c2ac30ad0ae964fe04
OltLatifi <oltlatifi2003@gmail.com>
2024-12-08 21:00:37 +0100

    Initial commit

---
+++ librarymenagment/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace librarymenagment.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
