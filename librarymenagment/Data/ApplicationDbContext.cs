1a3806d9208fa79aa8e730865343b8c6293342ff
Anisa <af69153@ubt-uni.net>
2025-01-26 00:56:07 -1100

    anisa's commit

--- librarymenagment/Data/ApplicationDbContext.cs
+++ librarymenagment/Data/ApplicationDbContext.cs
@@ -71,6 +71,7 @@ namespace librarymenagment.Data
         public DbSet<librarymenagment.Models.Available> Available { get; set; } = default!;
         public DbSet<UserBookPermission> UserBookPermission { get; set; }
         public DbSet<librarymenagment.Models.Member> Member { get; set; } = default!;
+        public DbSet<librarymenagment.Models.Events> Events { get; set; } = default!;
        
     }
 }
