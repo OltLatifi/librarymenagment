b4f9f7022d0cdb1dda7705c5f40816f2495fd585
Anisa <af69153@ubt-uni.net>
2025-01-26 00:01:16 -1100

    anisa's commit

---
+++ librarymenagment/Models/Members.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace librarymenagment.Models
+{
+    public class Member : BaseModel
+    {
+        public int Id { get; set; } 
+
+        public string FirstName { get; set; } 
+
+        public string LastName { get; set; } 
+
+        public DateTime DateOfBirth { get; set; } 
+
+        public string Email { get; set; } 
+
+        public string PhoneNumber { get; set; } 
+
+        public string Address { get; set; } 
+
+        public DateTime MembershipDate { get; set; } 
+
+        public string MembershipType { get; set; } 
+
+        public bool IsActive { get; set; } 
+
+        public int MaxBorrowLimit { get; set; }
+
+        public ICollection<Book> BorrowedBooks { get; set; }
+    }
+}
